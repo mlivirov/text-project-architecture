84d63113cdd0917d8be284956dbd9a40bc0ec2ef
mlivirov <max.livirov@gmail.com>
2025-10-21 22:28:51 +0300

    Remove old simple implementation

--- api/StupidChat/IChatRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface IChatRepository
-{
-    Task<Chat> GetByIdAsync(int id);
-    Task<int> AddAsync(Chat chat);
-    Task Update(int id, Chat chat);
-}
